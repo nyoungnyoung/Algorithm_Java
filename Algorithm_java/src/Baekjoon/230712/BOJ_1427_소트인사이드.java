4a62462030cee81b468d729003a5787ab1946ab3
sunysa8 <sunysa8@naver.com>
2023-07-12 02:17:00 +0900

    백준-1427.소트인사이드(선택정렬)

---
+++ Algorithm_java/src/Baekjoon/230712/BOJ_1427_소트인사이드.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1427_소트인사이드 {
+    // 선택정렬
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int[] A = new int[str.length()];
+        // 배열 A 저장
+        for (int i = 0; i < str.length(); i++) {
+            A[i] = Integer.parseInt(str.substring(i, i + 1));
+        }
+        // 선택정렬 수행(str 끝까지 돌아보면서 최댓값과 해당 인덱스의 값 바꿔주기
+        for (int i = 0; i < str.length(); i++) {
+            int Max = i;
+            for (int j = i + 1; j < str.length(); j++) {
+                if (A[j] > A[Max]) {
+                    Max = j;
+                }
+            }
+            if (A[i] < A[Max]) {
+                // 자리 바꿔주기(temp 선언해줘야 바꿀수 있음)
+                int temp = A[i];
+                A[i] = A[Max];
+                A[Max] = temp;
+            }
+        }
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(A[i]);
+        }
+    }
+}
